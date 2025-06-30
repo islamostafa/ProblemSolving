14bb5f93dc128a523c63a91fe169d963c290c928
Islam Mostafa <islam.maim@gmail.com>
2025-06-30 12:56:36 +0100

    Add ValidParentheses solution

---
+++ ValidParenthese.java
@@ -0,0 +1,26 @@
+import java.util.Stack;
+/**
+ * Problem: https://leetcode.com/problems/valid-parentheses/
+ * Difficulty: Easy
+ */
+public class ValidParenthese {
+
+    public boolean isValid(String s) {
+
+        if (s.length() % 2 != 0) return false;
+        Stack<Character> stack = new Stack<>();
+
+        for  (char parenthese : s.toCharArray() ) {
+            if (parenthese == '(' || parenthese == '['|| parenthese == '{') {
+                stack.push(parenthese);
+            } else {
+                if (stack.isEmpty())  return false;
+                char top = stack.pop();
+                if (parenthese ==')' && top != '(') return false;
+                if (parenthese == ']' && top != '[') return false;
+                if (parenthese == '}' && top != '{') return false;
+            }
+        }
+        return stack.isEmpty();
+    }
+}
